3d0670e0e89d5e69242ebcd522051f228d67878e
forseti <slickwolf@gmail.com>
2018-11-04 01:51:28 +0800

    Move optional parameters to the bottom

--- src/main/kotlin/com/jetpackr/configuration/Metadata.kt
+++ src/main/kotlin/com/jetpackr/configuration/Metadata.kt
@@ -7,9 +7,9 @@ import org.apache.commons.lang3.builder.ToStringStyle
 @JsonIgnoreProperties(ignoreUnknown = true)
 open class Metadata(
         val name: String,
-        val alias: String? = null,
         val label: String,
-        val description: String? = null
+        val description: String? = null,
+        val alias: String? = null
 ) {
     override fun toString(): String {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE)
