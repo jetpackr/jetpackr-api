1bb16e4d0512d32c1e027f1b678e9e0e8de5813e
forseti <slickwolf@gmail.com>
2018-11-07 01:22:41 +0800

    Complete implementation of SDKMANLoader

--- src/main/kotlin/com/jetpackr/common/loader/SDKMANLoader.kt
+++ src/main/kotlin/com/jetpackr/common/loader/SDKMANLoader.kt
@@ -1,5 +1,14 @@
 package com.jetpackr.common.loader
 
-val SDKMANLoader: SourceLoader = { _, _ ->
-    listOf()
+import com.jetpackr.common.comparator.VersionComparator
+import com.jetpackr.common.data.parameter.Option
+import io.ktor.client.request.get
+
+val SDKMANLoader: SourceLoader = { url, client ->
+    if (url == null)
+        throw Exception("Url cannot be empty")
+
+    client.get<String>(url).split(",").sortedWith(VersionComparator).map {
+        Option(value = it)
+    }
 }
