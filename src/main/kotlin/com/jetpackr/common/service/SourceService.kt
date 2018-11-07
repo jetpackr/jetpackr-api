987ae79303c64542342607182a567cbaa4defaad
forseti <slickwolf@gmail.com>
2018-11-07 18:26:58 +0800

    Improvise the code, remove the 'runBlocking'

--- src/main/kotlin/com/jetpackr/common/service/SourceService.kt
+++ src/main/kotlin/com/jetpackr/common/service/SourceService.kt
@@ -4,22 +4,16 @@ import com.jetpackr.common.data.parameter.Option
 import com.jetpackr.common.data.source.Source
 import com.jetpackr.common.loader.SourceLoader
 import io.ktor.client.HttpClient
-import kotlinx.coroutines.runBlocking
 
 class SourceService(
         private val sourceLoaders: Map<Source.Type, SourceLoader>,
         private val client: HttpClient
 ) {
-    init {
-        if (sourceLoaders == null)
-            throw RuntimeException("Source Loaders are empty")
-    }
-
-    fun load(source: Source): List<Option> = runBlocking {
-        val sourceLoader: SourceLoader? = sourceLoaders[source.type]
+    suspend fun load(source: Source): List<Option> {
+        val sourceLoader = sourceLoaders[source.type]
 
         if (sourceLoader != null)
-            sourceLoader.invoke(source.url, client)
+            return sourceLoader.invoke(source.url, client)
         else
             throw RuntimeException("${source.type}'s Source Loader is empty")
     }
