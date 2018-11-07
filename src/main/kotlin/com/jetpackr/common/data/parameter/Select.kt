30e4c7b19cce1a537dfbe53333d23920c9821bc9
forseti <slickwolf@gmail.com>
2018-11-07 18:23:23 +0800

    Remove Source

--- src/main/kotlin/com/jetpackr/common/data/parameter/Select.kt
+++ src/main/kotlin/com/jetpackr/common/data/parameter/Select.kt
@@ -1,14 +1,12 @@
 package com.jetpackr.common.data.parameter
 
-import com.jetpackr.common.data.source.Source
 import org.apache.commons.lang3.builder.ToStringBuilder
 import org.apache.commons.lang3.builder.ToStringStyle
 
 class Select(
         name: String,
         label: String,
-        val options: List<Option>,
-        val source: Source? = null
+        val options: List<Option>
 ): Parameter(
         name,
         label
