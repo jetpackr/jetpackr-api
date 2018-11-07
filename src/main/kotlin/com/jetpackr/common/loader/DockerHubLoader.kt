3cfaa860522e27af6dc2334daf80b08c6577c41a
forseti <slickwolf@gmail.com>
2018-11-07 18:28:59 +0800

    Complete implementation of DockerHubLoader

--- src/main/kotlin/com/jetpackr/common/loader/DockerHubLoader.kt
+++ src/main/kotlin/com/jetpackr/common/loader/DockerHubLoader.kt
@@ -1,5 +1,27 @@
 package com.jetpackr.common.loader
 
-val DockerHubLoader: SourceLoader = { _, _ ->
-    listOf()
+import com.jetpackr.common.comparator.VersionComparator
+import com.jetpackr.common.data.parameter.Option
+import com.jetpackr.common.data.response.DockerHubResponse
+import com.jetpackr.common.filter.VersionFilter
+import io.ktor.client.request.get
+
+val DockerHubLoader: SourceLoader = { url, client ->
+    val versions = mutableListOf<String>()
+    var response = client.get<DockerHubResponse>(url)
+
+    versions += response.results.map {
+        it["name"] as String
+    }
+
+    while (response.next != null) {
+        response = client.get(response.next!!)
+        versions += response.results.map {
+            it["name"] as String
+        }
+    }
+
+    versions.filter(VersionFilter).sortedWith(VersionComparator).map {
+        Option(value = it)
+    }
 }
