43f8c96c4a105e4549459fb1fc4f1086c2e5142a
forseti <slickwolf@gmail.com>
2018-11-07 18:10:36 +0800

    Add toString

--- src/main/kotlin/com/jetpackr/configuration/Jetpackr.kt
+++ src/main/kotlin/com/jetpackr/configuration/Jetpackr.kt
@@ -1,11 +1,15 @@
 package com.jetpackr.configuration
 
 import com.jetpackr.configuration.container.Container
-import com.jetpackr.configuration.tool.Tool
-import com.jetpackr.configuration.machine.Machine
+import org.apache.commons.lang3.builder.ToStringBuilder
+import org.apache.commons.lang3.builder.ToStringStyle
 
 class Jetpackr(
         val machine: Machine,
         val kits: Map<String, Tool>,
         val containers: Map<String, Container>
-)
+) {
+    override fun toString(): String {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE)
+    }
+}
