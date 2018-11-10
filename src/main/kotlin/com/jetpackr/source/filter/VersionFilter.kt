6a69cf4d33a4ae93592a8a13ddb9d5a76021be12
forseti <slickwolf@gmail.com>
2018-11-10 17:00:31 +0800

    Include only alpine images in addition to the standard images

--- src/main/kotlin/com/jetpackr/source/filter/VersionFilter.kt
+++ src/main/kotlin/com/jetpackr/source/filter/VersionFilter.kt
@@ -1,5 +1,5 @@
 package com.jetpackr.source.filter
 
 val VersionFilter: (String) -> Boolean = {
-    Regex("^v?[0-9]+([._][0-9]+)*([._-][A-Za-z0-9]+)*") matches it
+    Regex("^v?[0-9]+([._][0-9]+)*(-(alpine))?") matches it
 }
