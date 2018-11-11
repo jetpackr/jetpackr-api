6ed6f92df9be31ac56cce05714e71468409ce288
forseti <slickwolf@gmail.com>
2018-11-11 22:20:26 +0800

    Rename VersionFilter to SourceFilter and exclude any related Windows release

---
+++ src/main/kotlin/com/jetpackr/source/common/SourceFilter.kt
@@ -0,0 +1,8 @@
+package com.jetpackr.source.common
+
+val MATCHES = Regex("^v?\\d+([._]\\d+)*([._-]\\p{Alnum}+)*")
+val CONTAINS = Regex("(windowsservercore|nanoserver|deprecated)(\\p{Alnum})*")
+
+val SourceFilter: (String) -> Boolean = {
+   it matches MATCHES && !it.contains(regex = CONTAINS)
+}
--- src/main/kotlin/com/jetpackr/source/filter/VersionFilter.kt
+++
@@ -1,5 +0,0 @@
-package com.jetpackr.source.filter
-
-val VersionFilter: (String) -> Boolean = {
-    Regex("^v?[0-9]+([._][0-9]+)*(-(alpine))?") matches it
-}
