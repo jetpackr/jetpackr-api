031743fe3a73cf841ea2cfd786e1ad73fefebfdc
forseti <slickwolf@gmail.com>
2018-11-11 22:20:49 +0800

    Rename VersionComparator to SourceComparator

--- src/main/kotlin/com/jetpackr/source/common/SourceComparator.kt
+++ src/main/kotlin/com/jetpackr/source/common/SourceComparator.kt
@@ -1,8 +1,8 @@
-package com.jetpackr.source.comparator
+package com.jetpackr.source.common
 
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 
-val VersionComparator = Comparator<String> { version1: String, version2: String ->
+val SourceComparator = Comparator<String> { version1: String, version2: String ->
         DefaultArtifactVersion(
                 version2.replaceFirst("v","")
                         .replace("_", ".")
