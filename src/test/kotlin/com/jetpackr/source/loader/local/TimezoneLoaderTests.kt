4b38279b6752403af8286281ffc0e999b1fa8205
forseti <slickwolf@gmail.com>
2018-11-18 23:58:40 +0800

    Add tests for Timezone loader

--- src/test/kotlin/com/jetpackr/source/loader/local/TimezoneLoaderTests.kt
+++ src/test/kotlin/com/jetpackr/source/loader/local/TimezoneLoaderTests.kt
@@ -1,15 +1,72 @@
 package com.jetpackr.source.loader.local
 
+import io.kotlintest.Description
 import io.kotlintest.specs.StringSpec
+import io.mockk.every
+import io.mockk.spyk
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should contain all`
 
 class TimezoneLoaderTests : StringSpec() {
-    init {
-        "test on available timezones" {
-            TODO()
+    val log = KotlinLogging.logger {}
+
+    val mock = spyk<TimezoneLoader>()
+
+    val DEPRECATED_TIMEZONES = listOf(
+            "WINDOWS~Europe/Helsinki",
+            "MILITARY~UTC+12:00",
+            "Etc/GMT+10",
+            "MET",
+            "HST"
+    )
+
+    val VALID_TIMEZONES = listOf(
+            "Atlantic/Faroe",
+            "Asia/Nicosia",
+            "Pacific/Majuro",
+            "Europe/Tallinn",
+            "Indian/Chagos"
+    )
+
+    override fun beforeTest(description: Description) {
+        val name = description.name
+
+        if (name.contains("deprecated timezones")) {
+            every { mock["getZoneIds"]() } returns DEPRECATED_TIMEZONES
         }
 
-        "test on deprecated timezones" {
-            TODO()
+        if (name.contains("valid timezones")) {
+            every { mock["getZoneIds"]() } returns VALID_TIMEZONES
+        }
+    }
+    init {
+        "test deprecated timezones" {
+            runBlocking {
+                val timezones = mock.load()
+                log.info("releases: {}", timezones)
+
+                timezones.size `should be equal to` 0
+
+                Any()
+            }
+        }
+
+        "test valid timezones" {
+            runBlocking {
+                val timezones = mock.load()
+                log.info("releases: {}", timezones)
+
+                timezones.size `should be equal to` 5
+                timezones.values `should contain all` listOf(
+                        "Atlantic/Faroe",
+                        "Pacific/Majuro",
+                        "Indian/Chagos"
+                )
+
+                Any()
+            }
         }
     }
 }
