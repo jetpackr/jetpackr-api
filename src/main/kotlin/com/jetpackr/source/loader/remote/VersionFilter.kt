eefb8b9b75460484a4369c3d1c72f44761457304
forseti <slickwolf@gmail.com>
2018-11-18 23:44:46 +0800

    Make regexes private

--- src/main/kotlin/com/jetpackr/source/loader/remote/VersionFilter.kt
+++ src/main/kotlin/com/jetpackr/source/loader/remote/VersionFilter.kt
@@ -1,7 +1,7 @@
 package com.jetpackr.source.loader.remote
 
-val MATCHES = Regex("^v?\\d+([._]\\d+)*([._-]\\p{Alnum}+)*")
-val CONTAINS = Regex("(windowsservercore|nanoserver|deprecated)(\\p{Alnum})*")
+private val MATCHES = Regex("^v?\\d+([._]\\d+)*([._-]\\p{Alnum}+)*")
+private val CONTAINS = Regex("(windowsservercore|nanoserver|deprecated)(\\p{Alnum})*")
 
 val VersionFilter: (String) -> Boolean = {
    it matches MATCHES && !it.contains(regex = CONTAINS)
