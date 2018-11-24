f6c9a14eef804761cd8038c616084dab8a15fd9c
forseti <slickwolf@gmail.com>
2018-11-24 23:56:45 +0800

    Use second instead of first

--- src/main/kotlin/com/jetpackr/source/loader/remote/VersionComparator.kt
+++ src/main/kotlin/com/jetpackr/source/loader/remote/VersionComparator.kt
@@ -4,10 +4,10 @@ import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 
 val VersionComparator = Comparator<Pair<String, String>> { pair1: Pair<String, String>, pair2: Pair<String, String> ->
         DefaultArtifactVersion(
-                pair2.first.replaceFirst("v","")
+                pair2.second.replaceFirst("v","")
                                 .replace("_", ".")
         ).compareTo(DefaultArtifactVersion(
-                pair1.first.replaceFirst("v","")
+                pair1.second.replaceFirst("v","")
                                 .replace("_", ".")
         ))
 }
--- src/main/kotlin/com/jetpackr/source/loader/remote/VersionFilter.kt
+++ src/main/kotlin/com/jetpackr/source/loader/remote/VersionFilter.kt
@@ -4,5 +4,5 @@ private val MATCHES = Regex("^v?\\d+([._]\\d+)*([._-]\\p{Alnum}+)*")
 private val CONTAINS = Regex("(windowsservercore|nanoserver|deprecated|beta|alpha|preview|next)(\\p{Alnum})*")
 
 val VersionFilter: (Pair<String, String>) -> Boolean = {
-   it.first matches MATCHES && !it.first.contains(regex = CONTAINS)
+   it.second matches MATCHES && !it.second.contains(regex = CONTAINS)
 }
